bbe452e769f21f024086df6feed6187c7f5931b4
roberttong <roberttong@tencent.com>
2020-12-18 17:56:45 +0800

    common

--- pingcap/common/math.go
+++ pingcap/common/math.go
@@ -13,17 +13,20 @@ func IntToBytes(n int) []byte {
 }
 func BytesToInt(b []byte) int {
 	bytesBuffer := bytes.NewBuffer(b)
-
 	var x int32
 	binary.Read(bytesBuffer, binary.BigEndian, &x)
 
 	return int(x)
 }
-func ByteToInt64(bs []byte) int64 {
-	if len(bs) < 4 {
-		return 0
-	}
-	v := (int64(bs[7]) << 58) | (int64(bs[6]) << 48) | (int64(bs[5]) << 40) | (int64(bs[4]) << 32) |
-		(int64(bs[3]) << 24) | (int64(bs[2]) << 16) | (int64(bs[1]) << 8) | (int64(bs[0]))
-	return v
+
+func Int64ToBytes(n int64) []byte {
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, n)
+	return bytesBuffer.Bytes()
+}
+func BytesToInt64(b []byte) int64 {
+	bytesBuffer := bytes.NewBuffer(b)
+	var x int64
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+	return x
 }
---
+++ pingcap/common/math_test.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnit_Int(t *testing.T) {
+	te := assert.New(t)
+	var r = []struct {
+		n int
+	}{
+		{1},
+		{1 << 10},
+		{1 << 20},
+	}
+	for _, v := range r {
+		d := IntToBytes(v.n)
+		e := BytesToInt(d)
+		te.Equal(v.n, e)
+	}
+}
